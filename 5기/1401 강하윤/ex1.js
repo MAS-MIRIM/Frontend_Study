14430179c589cbadff990bb5fac7683d42a90b35
kanghayun <khydino@naver.com>
2025-09-03 19:27:02 +0900

    0903 강하윤

---
+++ 5기/1401 강하윤/ex1.js
@@ -0,0 +1,9 @@
+let user = "정희진";
+let score = 100;
+console.log(`${user}의 점수는 ${score}점입니다.`);
+console.log(user+"의 점수는 "+score+"점이라고여.");
+// alert("경고장입니다.");
+// let result = confirm("당신은 학생입니까?"); // 확인(true)   // 취소(false)
+// console.log(result);
+let age = prompt("나이를 입력하세요");
+console.log(age);
---
+++ 5기/1401 강하윤/ex3.js
@@ -0,0 +1,7 @@
+let gender = confirm("남자임? : ");
+if(gender==true){
+    console.log("그가 왔다.");
+}
+else{
+    console.log("그녀가 왔다.");
+}
